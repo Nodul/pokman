af60ac0a544293de0b7ddad9ef5eee1c42055e5d
Kamil Aniszkiewicz <kamil.aniszkiewicz@gmail.com>
2024-05-04 18:40:39 +0200

    checkpoint 2

--- src/Main.java
+++ src/Main.java
@@ -22,14 +22,14 @@ public class Main {
         var mainWindowFrame = new JFrame("Pokman");
 
         mainWindowFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        mainWindowFrame.setSize(600, 500);
+        var gridLayout = new GridLayout(12,12);//(12,12,0,0);
+       // gridLayout.
+        //mainWindowFrame.setSize(600, 500);
+        mainWindowFrame.setLayout(gridLayout);
         mainWindowFrame.setLocationRelativeTo(null);
-
-        var gridLayout = new GridLayout(12,12,0,0);//(12,12,0,0);
         // mainWindowFrame.setLayout(grid);
-        var mapContainerPanel = new JPanel(gridLayout);
-        mainWindowFrame.add(mapContainerPanel);
+       // var mapContainerPanel = new JPanel(gridLayout);
+       // mainWindowFrame.add(mapContainerPanel);
 
         //Create a panel and add components to it.
 //        JPanel contentPane = new JPanel(new BorderLayout());
@@ -64,22 +64,43 @@ public class Main {
         for(int y = 0; y < 12; y++){
             for(int x = 0; x < 12; x++){
                 var cell = map[x][y];
-                // var image = cell.GetIsWall() ? "X" : " "; // TODO after loading test wall, render it instead of the "X" or " "
+                JLabel picLabel = makeLabel(cell, wall_sprite, floor_sprite);
 
-                var icon = new ImageIcon(cell.GetIsWall() ? wall_sprite : floor_sprite);
+               // picLabel.setBorder(new LineBorder(Color.cyan));
 
-                JLabel picLabel = new JLabel(icon);
-                picLabel.setBorder(new LineBorder(Color.cyan));
+               // picLabel.setVisible(true);
 
-                picLabel.setVisible(true);
-
-                mapContainerPanel.add(picLabel);
+               // mapContainerPanel.add(picLabel);
+                mainWindowFrame.add(picLabel);
             }
         }
 
         // Finalize
-        mapContainerPanel.setVisible(true);
+        mainWindowFrame.pack();
         mainWindowFrame.setVisible(true);
+      //  mapContainerPanel.setVisible(true);
+    }
+
+    private static JLabel makeLabel(Cell cell, BufferedImage wall_sprite, BufferedImage floor_sprite){
+        JLabel label= new JLabel();
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setPreferredSize(new Dimension(28, 32));
+        var icon = new ImageIcon(cell.GetIsWall() ? wall_sprite : floor_sprite);
+
+        label.setIcon(icon);
+        label.setSize(icon.getIconWidth(), icon.getIconWidth());
+        label.setMaximumSize(new Dimension(28,32));
+        label.setBounds(0,0,28,32);// new Dimension(40,40));
+        //label.setIcon(icon);
+//        if(cell.GetIsWall()){
+//            label.setBackground(Color.blue);
+//        }
+//        else{
+//            label.setBackground(Color.white);
+//        }
+        label.setOpaque(true);
+        //label.setBorder(BorderFactory createLineBorder(Color.cyan, 0));
+        return label;
     }
 
     private static void RunLoop(){
